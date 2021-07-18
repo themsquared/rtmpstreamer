9f7dea3a9b7e51b90eccb886ba7c7bf35097ce59
MICHAEL MOORE <themsquared@users.noreply.github.com>
2021-07-19 08:47:00 +1000

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -5,4 +5,4 @@ echo Stream Resolution: $2
 echo Framerate $3
 echo Stream URL posting to: $4
 
-ffmpeg -re -f mjpeg -framerate $3 -i $1 -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -acodec aac -ab 128k -strict experimental -s $2 -vcodec h264 -pix_fmt yuv420p -g 10 -vb 700k -framerate $3 -c copy -metadata:s:v:0 rotate=180 -f flv -filter:v $5 $4
+ffmpeg -re -f mjpeg -framerate $3 -i $1 -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -acodec aac -ab 128k -strict experimental -s $2 -vcodec h264 -pix_fmt yuv420p -g 10 -vb 700k -framerate $3 -metadata:s:v:0 rotate=90 -f flv -filter:v $5 $4
